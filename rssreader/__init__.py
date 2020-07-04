7ff24e58067f73ed0e43b1efd11fd676ba1d0126
Robbe Van Herck <robbe@robbevanherck.be>
2020-07-04 10:47:03 +0200

    Add POST to /feeds

--- rssreader/__init__.py
+++ rssreader/__init__.py
@@ -1,42 +1,56 @@
-"""
-rssreader: The main Flask rssreader
-"""
+import functools
 
-# Import flask and template operators
-from flask import Flask, render_template
-
-# Import SQLAlchemy
+from flask import Flask, render_template, jsonify, request, Response
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
-# Define the WSGI application object
+from rssreader.helpers import accept
+
 app = Flask(__name__)
-
-# Configurations
 app.config.from_object('config')
+CORS(app)
 
-# Define the database object which is imported
-# by modules and controllers
 db = SQLAlchemy(app)
 
+from rssreader.models import Feed
 
-# Sample HTTP error handling
 @app.errorhandler(404)
 def not_found(error):
-    """
-    not_found: Handles HTTP error code 404 Not Found
-    """
     return render_template('404.html', error=error), 404
 
-
-# Set the route and accepted methods
 @app.route('/', methods=['GET'])
+@accept
 def root():
-    """
-    root: The root route
-    """
     return render_template("root.html")
 
+@root.accept('application/json')
+def root_json():
+    return jsonify({})
 
-# Build the database:
-# This will create the database file using SQLAlchemy
+@app.route('/feeds', methods=['GET', 'POST'])
+@accept
+def feeds():
+    return render_template("todo.html")
+
+@feeds.accept('application/json')
+def feeds_json():
+    if request.method == 'GET':
+        return jsonify([x.json() for x in Feed.query.all()])
+    elif request.method == 'POST':
+        new_feed = request.get_json()
+        if 'name' not in new_feed or new_feed['name'] == '':
+            return error("Missing field 'name'")
+        if 'url' not in new_feed or new_feed['url'] == '':
+            return error("Missing field 'url'")
+        feed = Feed()
+        feed.name = new_feed['name']
+        feed.url = new_feed['url']
+        db.session.add(feed)
+        db.session.commit()
+        return jsonify(feed.json())
+
+
+def error(description, status=400):
+    return Response(description, status=status)
+
 db.create_all()
--- rssreader/models.py
+++ rssreader/models.py
@@ -1,6 +1,38 @@
 """
-models.py
+models.py: The SQLAlchemy models
 """
 
-# Import the database object (db) from the main application module
-# We will define this inside /rssreader/__init__.py in the next sections.
+import rssreader
+
+class Feed(rssreader.db.Model):
+    __tablename__ = 'feed'
+    id = rssreader.db.Column(rssreader.db.Integer, primary_key=True)
+    name = rssreader.db.Column(rssreader.db.String(64), nullable=False)
+    url = rssreader.db.Column(rssreader.db.String(128), nullable=False)
+    priority = rssreader.db.Column(rssreader.db.Integer, nullable=True, default=1)
+    hidden = rssreader.db.Column(rssreader.db.Boolean, default=False)
+    posts = rssreader.db.relationship("Post")
+
+    def __repr__(self):
+        return '<Feed %r>' % self.name
+
+    def json(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "url": self.url,
+            "priority": self.priority,
+            "hidden": self.hidden
+        }
+
+class Post(rssreader.db.Model):
+    __tablename__ = 'post'
+    id = rssreader.db.Column(rssreader.db.Integer, primary_key=True)
+    title = rssreader.db.Column(rssreader.db.String(32), nullable=False)
+    description = rssreader.db.Column(rssreader.db.String(1024), nullable=False)
+    link = rssreader.db.Column(rssreader.db.String(128), nullable=True)
+    read = rssreader.db.Column(rssreader.db.Boolean, default=False)
+    feed = rssreader.db.Column(rssreader.db.Integer, rssreader.db.ForeignKey('feed.id'))
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
