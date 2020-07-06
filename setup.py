a1864ab5e8bb529e4b1fb5808da1ade4a2c71a31
Robbe Van Herck <robbe@robbevanherck.be>
2020-07-06 13:41:06 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='RSSReader',
+      version='1.0',
+      description='Python RSS aggregator and API',
+      author='Robbe Van Herck',
+      author_email='robbe@robbevanherck.be',
+      url='https://github.com/Robbe7730/RSSReader',
+      packages=['rssreader'],
+     )
